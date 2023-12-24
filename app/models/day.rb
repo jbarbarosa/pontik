508d9e6bdd01a7493d0f8a4be07c5e63ecd9f51d
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2023-12-24 11:08:24 -0300

    calculates worked hours
    test green, now refactoring

--- app/models/day.rb
+++ app/models/day.rb
@@ -17,7 +17,14 @@ class Day
     "#{of_the_week.downcase}-#{of_the_month}"
   end
 
-  def worked_hours
-    '0h00m'
+  def worked_hours(user)
+    times = Clock.where(user:, time: @date.beginning_of_day..@date.end_of_day).pluck(:time)
+    if times.length > 1
+      hours = (times[1] - times[0]) / 60 / 60
+      minutes = (times[1] - times[0]) % 60
+      "#{format('%02d', hours.to_i)}h#{format('%02d', minutes.to_i)}m"
+    else
+      '00h00m'
+    end
   end
 end
