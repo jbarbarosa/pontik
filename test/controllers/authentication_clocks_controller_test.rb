87922d0f92474361c5ca02ffa629dd73a1021c60
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2024-01-13 18:25:17 -0300

    tests authentication for all clock routes
    using a pattern common in golang, we can test multiple inputs (routes) without code duplication

--- test/controllers/authentication_clocks_controller_test.rb
+++ test/controllers/authentication_clocks_controller_test.rb
@@ -5,7 +5,10 @@ class AuthenticationClocksControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   test "should be redirected to the login screen if not currently authenticated" do
-    get clocks_url
-    assert_redirected_to user_session_path
+    # A Table Test reduces code duplication when the expectation is the same for different inputs that must be tested separetely
+    [clocks_url, edit_clock_url(clocks(:one)), new_clock_url, clock_url(clocks(:one))].each do |url| 
+      get url
+      assert_redirected_to user_session_path, "unauthenticated request to #{url} did not redirect to sign_in path"
+    end
   end
 end
